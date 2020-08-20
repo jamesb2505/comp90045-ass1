a0fb4ab376295245c9051c8049f06e60582e89a5
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2020-08-20 14:00:38 +1000

    feat: added files

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for Roo
+
+## Unreleased changes
---
+++ src/PrettyJoey.hs
@@ -0,0 +1,9 @@
+module PrettyJoey (pp) where
+
+import JoeyAST
+
+--  Pretty print a whole program:
+
+pp :: Program -> String
+pp _ =
+  "Very pretty!"

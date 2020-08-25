79338f83f1a94dc6d0548b562ad14647a06102bc
James Barnes <jamesbarnes2505@gmail.com>
2020-08-25 22:27:37 +1000

    cleaned up Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -12,38 +12,27 @@ data Task
   deriving (Eq, Show)
 
 main :: IO ()
-main =
-  do
-    progname <- getProgName
-    args <- getArgs
-    task <- checkArgs progname args
-    case task of
-      Parse ->
-        do
-          let [_, filename] = args
-          input <- readFile filename
-          doParse show input
-      Pprint ->
-        do
-          let [_, filename] = args
-          input <- readFile filename
-          doParse pprint input
+main = do
+  progname <- getProgName
+  args <- getArgs
+  task <- checkArgs progname args
+  case task of
+    Parse -> doParse show args
+    Pprint -> doParse pprint args
 
 checkArgs :: String -> [String] -> IO Task
-checkArgs _ ['-' : _] =
-  do
-    putStrLn ("Missing filename")
-    exitWith (ExitFailure 1)
+checkArgs _ ['-' : _] = 
+  doErr ("Missing filename") 1
 checkArgs _ ["-a", filename] =
   return Parse
 checkArgs _ ["-p", filename] =
   return Pprint
 checkArgs progname _ =
-  do
-    putStrLn ("Usage: " ++ progname ++ " [-p] filename")
-    exitWith (ExitFailure 1)
+  doErr ("Usage: " ++ progname ++ " [-p] filename") 1
 
-doParse f input =
+doParse f args = do
+  let [_, filename] = args
+  input <- readFile filename
   case runLexer input >>= runParser of
     Left err -> doErr err 2
     Right parsed -> putStrLn $ f parsed

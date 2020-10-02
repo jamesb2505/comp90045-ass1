544fdbfbe6ab4a036efaec123b8872f9af4d7b30
James Barnes <jamesbarnes2505@gmail.com>
2020-10-02 21:01:10 +1000

    WIP: added procedure table

--- src/RooSymTable.hs
+++ src/RooSymTable.hs
@@ -6,17 +6,27 @@ import qualified Data.Containers.ListUtils as LU
 import qualified Control.Monad as C
 import qualified Data.Maybe as M
 
-import Debug.Trace
+p = AST.Program 
+      [AST.Record [AST.Field AST.IntType "i"] "r"] 
+      [AST.Array 10 (AST.Alias "r") "a"] 
+      [ AST.Procedure "main" [] [] [AST.Writeln (AST.StrConst "hello")]
+      , AST.Procedure "roo" [AST.ParamBase AST.IntType AST.Ref "i"] 
+                            [AST.Var (AST.Alias "r") ["h"]]
+                            [AST.Assign (AST.LId "i") (AST.IntConst 0)]]
+
+type Entry a = (AST.Ident, a)
+
+type Table a = [Entry a]
 
 data RooSymTable 
-  = RooSymTable { unRecords    :: [(AST.Ident, Record)]
-                , unArrays     :: [(AST.Ident, Array)]
-                , unProcedures :: [(AST.Ident, Procedure)]
+  = RooSymTable { unRecords    :: Table Record
+                , unArrays     :: Table Array
+                , unProcedures :: Table Procedure
                 }
   deriving (Show, Eq)
 
 data Record
-  = Record { unFields :: [(AST.Ident, Field)] 
+  = Record { unFields :: Table Field 
            }
   deriving (Show, Eq)
 
@@ -27,8 +37,8 @@ data Array
   deriving (Show, Eq)
 
 data Procedure
-  = Procedure { unParams :: [(AST.Ident, Variable)]
-              , unVars   :: [(AST.Ident, Variable)]
+  = Procedure { unParams :: Table Parameter
+              , unVars   :: Table Variable
               }
   deriving (Show, Eq)
 
@@ -38,9 +48,17 @@ data Field
           }
   deriving (Show, Eq)
 
+data Parameter 
+  = Parameter { unPType   :: AST.TypeName
+              , unPName   :: AST.Ident
+              , unMode    :: AST.Mode
+              , unPOffset :: Int
+              }
+  deriving (Show, Eq)
+
 data Variable 
-  = Variable { unVType   :: AST.BaseType
-             , unMode    :: AST.Mode
+  = Variable { unVType   :: AST.TypeName
+             , unVName   :: AST.Ident
              , unVOffset :: Int
              }
   deriving (Show, Eq)
@@ -56,53 +74,85 @@ symTable (AST.Program rs as ps)
      then Right table
      else Left "error in functions"
 
-recordTable :: [AST.Record] -> Either String [(AST.Ident, Record)]
+recordTable :: [AST.Record] -> Either String (Table Record)
 recordTable rs = 
   do 
-    records <- C.sequence (map convert rs)
+    records <- C.sequence (map convertRecord rs)
     checkDuplicates (map fst records) "duplicate record identifier" records
   where
-    convert (AST.Record fs ident) = 
-      let fields   = convertFields fs in
+    convertRecord :: AST.Record -> Either String (Entry Record)
+    convertRecord (AST.Record fs ident) = 
+      let fields = convertFields fs in
       checkDuplicates (map fst fields)
                       ("duplicate field identifier in `" ++ ident ++ "`")
                       (ident, Record fields)
-
-convertFields :: [AST.Field] -> [(AST.Ident, Field)]
-convertFields fs = zipWith convert fs [0..] 
-  where convert (AST.Field t ident) offset = (ident, Field t offset)
+    convertFields :: [AST.Field] -> [Entry Field]
+    convertFields fs = zipWith convertField fs [0..] 
+    convertField :: AST.Field -> Int -> Entry Field
+    convertField (AST.Field t ident) offset = (ident, Field t offset)
 
 arrayTable
-  :: [(AST.Ident, Record)]
-  -> [AST.Array] -> Either String [(AST.Ident, Array)]
+  :: Table Record
+  -> [AST.Array] -> Either String (Table Array)
 arrayTable rs as = 
   do
-    arrays <- C.sequence $ map convert as
+    arrays <- C.sequence $ map convertArray as
     checkDuplicates (aliases ++ map fst arrays)
                     ("duplcate type alias")
                     arrays
   where
-    convert (AST.Array size t ident) = 
+    convertArray :: AST.Array -> Either String (Entry Array)
+    convertArray (AST.Array size t ident) = 
       if size <= 0
       then Left $ "non-positive array size (" ++ show size 
                   ++ ") for `" ++ ident ++ "`"
-      else if not $ validType t
+      else if not $ validType aliases t
       then Left $ "invalid type for `" ++ ident ++ "`"
       else Right (ident, Array t (fromInteger size))
-    validType (AST.Base _)  = True
-    validType (AST.Alias a) = elem a aliases
+    aliases :: [AST.Ident]
     aliases = map fst rs
 
 procedureTable 
-  :: [(AST.Ident, Record)] -> [(AST.Ident, Array)] 
-  -> [AST.Procedure] -> Either String [(AST.Ident, Procedure)]
-procedureTable = undefined
+  :: Table Record -> Table Array 
+  -> [AST.Procedure] -> Either String (Table Procedure)
+procedureTable rs as ps = 
+  do
+    procedures <- C.sequence $ map convertProcedure ps
+    checkDuplicates (map fst procedures)
+                    "duplcate type alias"
+                    procedures
+  where
+    convertProcedure :: AST.Procedure -> Either String (Entry Procedure)
+    convertProcedure (AST.Procedure ident params vars _) =
+      let parameters = convertParams params
+          variables  = convertVars vars in
+      checkDuplicates (map fst variables ++ map fst parameters)
+                      ("duplcate parameter/variable name in `" ++ ident ++ "`")
+                      (ident, Procedure parameters variables)
+    convertParams :: [AST.Param] -> [(Entry Parameter)]
+    convertParams params = zipWith convertParam params [0..]
+    convertParam :: AST.Param -> Int -> Entry Parameter
+    convertParam (AST.ParamAlias t ident) offset = 
+      (ident, Parameter (AST.Alias t) ident AST.Ref offset)
+    convertParam (AST.ParamBase t m ident) offset = 
+      (ident, Parameter (AST.Base t) ident m offset)
+    convertVars :: [AST.Var] -> [Entry Variable]
+    convertVars vars = zipWith (\v i -> (unVName v, v { unVOffset = i })) 
+                         (concatMap convertVar vars) [0..]
+    convertVar :: AST.Var -> [Variable]
+    convertVar (AST.Var t is) = map (convertVar' t) is
+    convertVar' :: AST.TypeName -> AST.Ident -> Variable
+    convertVar' t i = Variable t i (-1) -- fixed in convertVars
 
 checkProcedures :: RooSymTable -> Bool
-checkProcedures = undefined
+checkProcedures = const True
 
 checkDuplicates :: (Ord c) => [c] -> a -> b -> Either a b 
 checkDuplicates toCheck left right = 
   if length (LU.nubOrd toCheck) == length toCheck
   then Right right
   else Left left
+
+validType :: [AST.Ident] -> AST.TypeName -> Bool
+validType _       (AST.Base _)  = True
+validType aliases (AST.Alias a) = elem a aliases

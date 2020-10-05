9a59f89c56b1c9d0e06953b0bab759b15dbf6167
James Barnes <jamesbarnes2505@gmail.com>
2020-10-05 21:21:37 +1100

    WIP: added to genExpr code; added to genStmt code

--- src/RooOzCodeGen.hs
+++ src/RooOzCodeGen.hs
@@ -50,8 +50,8 @@ data OzCode
   | Oz_mul_real RegNum RegNum RegNum
   | Oz_div_int RegNum RegNum RegNum
   | Oz_div_real RegNum RegNum RegNum
-  | Oz_neg_int RegNum RegNum RegNum
-  | Oz_neg_real RegNum RegNum RegNum
+  | Oz_neg_int RegNum RegNum
+  | Oz_neg_real RegNum RegNum
   | Oz_cmp_eq_int RegNum RegNum RegNum
   | Oz_cmp_ne_int RegNum RegNum RegNum
   | Oz_cmp_gt_int RegNum RegNum RegNum
@@ -88,83 +88,83 @@ instance Show OzCode where
   show (Oz_pop_stack_frame i)    
     = "  pop_stack_frame " ++ show i
   show (Oz_store s r)            
-    = "  store " ++ intercalate ", " [show r, show s]
+    = "  store " ++ intercalate ", " [ show r, show s ]
   show (Oz_load r s)             
-    = "  load " ++ intercalate ", " [show r, show s]
+    = "  load " ++ intercalate ", " [ show r, show s ]
   show (Oz_load_address r s)     
-    = "  load_address " ++ intercalate ", " [show r, show s]
+    = "  load_address " ++ intercalate ", " [ show r, show s ]
   show (Oz_load_indirect rI rJ)  
-    = "  load_indirect " ++ intercalate ", " (map show [rI, rJ])
+    = "  load_indirect " ++ intercalate ", " (map show [ rI, rJ ])
   show (Oz_store_indirect rI rJ) 
-    = "  store_indirect " ++ intercalate ", " (map show [rI, rJ])
+    = "  store_indirect " ++ intercalate ", " (map show [ rI, rJ ])
   show (Oz_int_const r i)       
-    = "  int_const " ++ intercalate ", " [show r, show i]
+    = "  int_const " ++ intercalate ", " [ show r, show i ]
   show (Oz_real_const r d)      
-    = "  real_const " ++ intercalate ", " [show r, show d]
+    = "  real_const " ++ intercalate ", " [ show r, show d ]
   show (Oz_string_const r s)    
     = "  string_const " ++ show r ++ ", \"" ++ s ++ "\"" 
   show (Oz_add_int rI rJ rK)
-    = "  add_int " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  add_int " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_add_real rI rJ rK)
-    = "  add_real " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  add_real " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_add_offset rI rJ rK)
-    = "  add_offset " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  add_offset " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_sub_int rI rJ rK)
-    = "  sub_int " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  sub_int " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_sub_real rI rJ rK)
-    = "  sub_real " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  sub_real " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_sub_offset rI rJ rK)
-    = "  sub_offset " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  sub_offset " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_mul_int rI rJ rK)
-    = "  mul_int " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  mul_int " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_mul_real rI rJ rK)
-    = "  mul_real " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  mul_real " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_div_int rI rJ rK)
-    = "  div_int " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  div_int " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_div_real rI rJ rK)
-    = "  div_real " ++ intercalate ", " (map show [rI, rJ, rK])
-  show (Oz_neg_int rI rJ rK)
-    = "  neg_int " ++ intercalate ", " (map show [rI, rJ, rK])
-  show (Oz_neg_real rI rJ rK)
-    = "  neg_real " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  div_real " ++ intercalate ", " (map show [ rI, rJ, rK ])
+  show (Oz_neg_int rI rJ)
+    = "  neg_int " ++ intercalate ", " (map show [ rI, rJ ])
+  show (Oz_neg_real rI rJ)
+    = "  neg_real " ++ intercalate ", " (map show [ rI, rJ ])
   show (Oz_cmp_eq_int rI rJ rK)
-    = "  cmp_eq_int " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  cmp_eq_int " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_cmp_ne_int rI rJ rK)
-    = "  cmp_ne_int " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  cmp_ne_int " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_cmp_gt_int rI rJ rK)
-    = "  cmp_gt_int " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  cmp_gt_int " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_cmp_ge_int rI rJ rK)
-    = "  cmp_ge_int " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  cmp_ge_int " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_cmp_lt_int rI rJ rK)
-    = "  cmp_lt_int " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  cmp_lt_int " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_cmp_le_int rI rJ rK)
-    = "  cmp_le_int " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  cmp_le_int " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_cmp_eq_real rI rJ rK)
-    = "  cmp_eq_real " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  cmp_eq_real " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_cmp_ne_real rI rJ rK)
-    = "  cmp_ne_real " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  cmp_ne_real " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_cmp_gt_real rI rJ rK)
-    = "  cmp_gt_real " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  cmp_gt_real " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_cmp_ge_real rI rJ rK)
-    = "  cmp_ge_real " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  cmp_ge_real " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_cmp_lt_real rI rJ rK)
-    = "  cmp_lt_real " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  cmp_lt_real " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_cmp_le_real rI rJ rK)
-    = "  cmp_le_real " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  cmp_le_real " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_and rI rJ rK)
-    = "  and " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  and " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_or rI rJ rK)
-    = "  or " ++ intercalate ", " (map show [rI, rJ, rK])
+    = "  or " ++ intercalate ", " (map show [ rI, rJ, rK ])
   show (Oz_not rI rJ)            
-    = "  not " ++ intercalate ", " (map show [rI, rJ])
+    = "  not " ++ intercalate ", " (map show [ rI, rJ ])
   show (Oz_int_to_real rI rJ)    
-    = "  int_to_real " ++ intercalate ", " (map show [rI, rJ])
+    = "  int_to_real " ++ intercalate ", " (map show [ rI, rJ ])
   show (Oz_move rI rJ)           
-    = "  move " ++ intercalate ", " (map show [rI, rJ])
+    = "  move " ++ intercalate ", " (map show [ rI, rJ ])
   show (Oz_branch_on_true r l)  
-    = "  branch_on_true " ++ intercalate ", " [show r, show l]
+    = "  branch_on_true " ++ intercalate ", " [ show r, show l ]
   show (Oz_branch_on_false r l) 
-    = "  branch_on_false " ++ intercalate ", " [show r, show l]
+    = "  branch_on_false " ++ intercalate ", " [ show r, show l ]
   show (Oz_branch_uncond l)      
     = "  branch_uncond " ++ show l
   show (Oz_label l)               
@@ -184,12 +184,12 @@ instance Show OzCode where
   show (Oz_debug_stack)          
     = "  debug_stack"
 
-nextLabel :: GenState LabelNum
+nextLabel :: GenState Label
 nextLabel =
   do
     Gen ln rn <- get
     put $ Gen (ln + 1) rn
-    return ln
+    return $ BranchLabel ln
 
 getRegister :: GenState RegNum
 getRegister = 
@@ -202,6 +202,14 @@ nextRegister =
   do
     Gen ln r@(Reg rn) <- get
     put . Gen ln . Reg $ rn + 1
+    return r
+
+prevRegister :: GenState RegNum
+prevRegister = 
+  do
+    Gen ln (Reg rn) <- get
+    let r = Reg $ rn - 1 
+    put $ Gen ln r
     return r
 
 putRegister :: Int -> GenState ()
@@ -219,13 +227,13 @@ runCodeGen prog st =
     case evalState (genProg st prog) initState of
       code -> return code
       
-repeatM :: (Monad m) => (a -> m [b]) -> [a] -> m [b]
-repeatM f = liftM mconcat . mapM f
+repeatGen :: (a -> GenState [OzCode]) -> [a] -> GenState [OzCode]
+repeatGen gen = liftM concat . mapM gen
     
 genProg :: ST.SymbolTable -> AST.Program -> GenState [OzCode]
 genProg st (AST.Program _ _ ps) =
   do 
-    procs <- repeatM (genProc st) ps
+    procs <- repeatGen (genProc st) ps
     return $ [ Oz_call (ProcLabel "main"), Oz_halt ] ++ procs
 
 genProc :: ST.SymbolTable -> AST.Procedure -> GenState [OzCode]
@@ -233,13 +241,13 @@ genProc st@(ST.SymbolTable _ _ ps) (AST.Procedure name _ _ ss) =
   do 
     let (ST.Procedure params vars stackSize) = fromJust $ lookup name ps
     let nParams = length params
-    stmts <- repeatM (genStmt st) ss
+    stmts <- repeatGen (genStmt st) ss
     if stackSize > 0
     then return $ Oz_label (ProcLabel name)
                 : Oz_push_stack_frame stackSize
                 -- TODO: put params on stack
                 : Oz_int_const (Reg 0) 0
-                : take stackSize [ Oz_store i $ Reg 0 | i <- [nParams..] ]
+                : [ Oz_store (i + nParams) $ Reg 0 | i <- [ 0 .. stackSize ] ]
                ++ stmts 
                ++ [ Oz_pop_stack_frame stackSize
                   , Oz_return
@@ -250,63 +258,146 @@ genProc st@(ST.SymbolTable _ _ ps) (AST.Procedure name _ _ ss) =
                ++ [ Oz_return ]
 
 genStmt :: ST.SymbolTable -> AST.Stmt -> GenState [OzCode]
-genStmt st (AST.Assign _ _) = -- TODO
+genStmt st (AST.Assign l e) =
   do 
-    return []
+    putRegister 0
+    eCode <- genExpr st e
+    putRegister 1
+    lCode <- genLValue st l
+    return $ eCode 
+          ++ lCode
+          ++ [ Oz_store_indirect (Reg 1) (Reg 0) ]
 genStmt st (AST.Read _) = -- TODO
   do 
+    putRegister 0
     return []
 genStmt st (AST.Write e) =
   do 
+    putRegister 0
     expr <- genExpr st e
     return $ expr 
-          ++ [ Oz_call_builtin "print_string" ]
+          ++ [ Oz_call_builtin . getPrintBuiltin $ AST.getExprT e ]
 genStmt st (AST.Writeln e) =
   do 
+    putRegister 0
     expr <- genExpr st e
     return $ expr
-          ++ [ Oz_call_builtin "print_string"
+          ++ [ Oz_call_builtin . getPrintBuiltin $ AST.getExprT e 
              , Oz_call_builtin "print_newline"
              ]
-genStmt st (AST.If _ _) = -- TODO
+genStmt st (AST.If e ss) =
   do 
-    return []
-genStmt st (AST.IfElse _ _ _) = -- TODO
+    putRegister 0
+    eCode <- genExpr st e
+    putRegister 0
+    ssCode <- repeatGen (genStmt st) ss
+    endLabel <- nextLabel
+    return $ eCode
+          ++ [ Oz_branch_on_false (Reg 0) endLabel ]
+          ++ ssCode
+          ++ [ Oz_label endLabel ]
+genStmt st (AST.IfElse e ts fs) =
   do 
-    return []
-genStmt st (AST.While _ _) = -- TODO
+    putRegister 0
+    eCode <- genExpr st e
+    putRegister 0
+    tsCode <- repeatGen (genStmt st) ts
+    putRegister 0
+    fsCode <- repeatGen (genStmt st) fs
+    falseLabel <- nextLabel
+    endLabel <- nextLabel
+    return $ eCode
+          ++ [ Oz_branch_on_false (Reg 0) falseLabel ]
+          ++ tsCode
+          ++ [ Oz_branch_uncond endLabel ]
+          ++ fsCode
+          ++ [ Oz_label endLabel ]
+genStmt st (AST.While e ss) =
   do 
-    return []
+    putRegister 0
+    eCode <- genExpr st e
+    putRegister 0
+    ssCode <- repeatGen (genStmt st) ss
+    startLabel <- nextLabel
+    endLabel <- nextLabel
+    return $ Oz_label startLabel
+           : eCode
+          ++ [ Oz_branch_on_false (Reg 0) endLabel ]
+          ++ ssCode
+          ++ [ Oz_branch_uncond startLabel
+             , Oz_label endLabel
+             ]
 genStmt st (AST.Call _ _) = -- TODO
   do 
+    putRegister 0
     return []
+
+genLValue :: ST.SymbolTable -> AST.LValue -> GenState [OzCode]
+genLValue _ _ = undefined -- TODO
 
 genExpr :: ST.SymbolTable -> AST.Expr -> GenState [OzCode]
 genExpr _ (AST.LVal _ _) = -- TODO
   do 
     return []
-genExpr _ (AST.BoolConst _ b) = -- TODO
+genExpr _ (AST.BoolConst _ b) =
   do 
-    r <- getRegister
-    return [Oz_int_const r (if b then 1 else 0)]
+    r <- nextRegister
+    return [ Oz_int_const r $ if b then 1 else 0 ]
 genExpr _ (AST.IntConst _ i) =
   do 
-    r <- getRegister
-    return [Oz_int_const r (fromInteger i)]
+    r <- nextRegister
+    return [ Oz_int_const r (fromInteger i) ]
 genExpr _ (AST.StrConst _ s) =
   do 
-    r <- getRegister
-    return [Oz_string_const r s]
-genExpr _ (AST.BinOpExpr _ _ _ _) = -- TODO
+    r <- nextRegister
+    return [ Oz_string_const r s ]
+genExpr st (AST.BinOpExpr _ op a b) =
   do 
-    return []
-genExpr _ (AST.UnOpExpr _ _ _) = -- TODO
+    r@(Reg n) <- getRegister
+    aCode <- genExpr st a
+    bCode <- genExpr st b
+    putRegister (n + 1)
+    r' <- getRegister
+    return $ aCode
+          ++ bCode
+          ++ [ getBinOpCode op r r r' ]
+genExpr st (AST.UnOpExpr _ op a) =
   do 
-    return []
+    r@(Reg n) <- getRegister
+    aCode <- genExpr st a
+    putRegister (n + 1)
+    return $ aCode
+          ++ [ getUnOpCode op r r ]
+
+getPrintBuiltin :: AST.ExprType -> String
+getPrintBuiltin AST.BoolT = "print_bool"
+getPrintBuiltin AST.IntT  = "print_int"
+getPrintBuiltin AST.StrT  = "print_string"
+getPrintBuiltin _         = error "no such builtin print"
+
+getBinOpCode :: AST.BinOp -> (RegNum -> RegNum -> RegNum -> OzCode)
+getBinOpCode Op_or  = Oz_or
+getBinOpCode Op_and = Oz_and
+getBinOpCode Op_eq  = Oz_cmp_eq_int
+getBinOpCode Op_neq = Oz_cmp_ne_int
+getBinOpCode Op_lt  = Oz_cmp_lt_int
+getBinOpCode Op_leq = Oz_cmp_le_int
+getBinOpCode Op_gt  = Oz_cmp_gt_int
+getBinOpCode Op_geq = Oz_cmp_ge_int
+getBinOpCode Op_add = Oz_add_int
+getBinOpCode Op_sub = Oz_sub_int
+getBinOpCode Op_mul = Oz_mul_int
+getBinOpCode Op_div = Oz_div_int
+
+getUnOpCode :: AST.UnOp -> (RegNum -> RegNum -> OzCode)
+getUnOpCode Op_not = Oz_not
+getUnOpCode Op_neg = Oz_neg_int
 
 p = "procedure main () { writeln \"Hello, World!\"; }"
-pr = AST.Program [] [] [ AST.Procedure "main" [] [] [AST.Writeln (AST.StrConst AST.StrT "Hello, World!")]
-                       , AST.Procedure "b" [] [] [AST.Writeln (AST.StrConst AST.StrT "Hello, World!")] -- incomplete
+pr = AST.Program [] [] [ AST.Procedure "main" [] [] [AST.Writeln (AST.BinOpExpr AST.IntT AST.Op_add (AST.IntConst IntT 0) (AST.IntConst IntT 0))]
+                       , AST.Procedure "b" [] [] [ AST.Writeln (AST.BinOpExpr AST.BoolT AST.Op_and (AST.UnOpExpr AST.BoolT AST.Op_not (AST.BoolConst BoolT True)) (AST.BoolConst AST.BoolT False))
+                                                 , AST.While (AST.BoolConst AST.BoolT False) [AST.Writeln (AST.StrConst AST.StrT "Hello, World!")]
+                                                 ] -- incomplete
                        ]
 st = ST.SymbolTable {ST.unRecords = [], ST.unArrays = [], ST.unProcedures = [ ("main",ST.Procedure {ST.unParams = [], ST.unVars = [], ST.unStackSize = 0})
                                                                             , ("b",ST.Procedure {ST.unParams = [], ST.unVars = [], ST.unStackSize = 1})
